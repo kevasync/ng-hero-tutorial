3600971d86287069ab90aa4f9949a51b8a2fb9b3
kevin.tinn <kevin.tinn@asynchrony.com>
2017-05-13 16:18:27 -0500

    Removed unused hero files from source

---
+++ src/app/skill-record.js
@@ -0,0 +1,26 @@
+"use strict";
+var Value = (function () {
+    function Value() {
+    }
+    return Value;
+}());
+exports.Value = Value;
+var SkillRecord = (function () {
+    function SkillRecord() {
+    }
+    return SkillRecord;
+}());
+exports.SkillRecord = SkillRecord;
+var Row = (function () {
+    function Row() {
+    }
+    return Row;
+}());
+exports.Row = Row;
+var CouchDbResponse = (function () {
+    function CouchDbResponse() {
+    }
+    return CouchDbResponse;
+}());
+exports.CouchDbResponse = CouchDbResponse;
+//# sourceMappingURL=skill-record.js.map
